944fe7712f901a2a6a8ea803849f5af6ce7573c3
Connor Darling <connordarling@me.com>
2026-03-09 19:58:36 -0700

    Add BasicRealm, Grid, and TimeRule classes; update Realm interface
    - Introduced BasicRealm class implementing the Realm interface, encapsulating realm properties such as ID, name, description, time rule, and grid.
    - Added Grid class to represent the dimensions of the realm.
    - Created TimeRule class to manage time offset and multiplier functionalities.
    - Updated Realm interface to define methods for accessing realm properties.

---
+++ src/main/java/edu/uci/inf122/guildquest/content/Grid.java
@@ -0,0 +1,19 @@
+package edu.uci.inf122.guildquest.content;
+
+public class Grid {
+    private int width;
+    private int height;
+
+    public Grid(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
---
+++ src/main/java/edu/uci/inf122/guildquest/content/TimeRule.java
@@ -0,0 +1,27 @@
+package edu.uci.inf122.guildquest.content;
+
+public class TimeRule {
+    private int offsetMinutes;
+    private double multiplier;
+
+    public TimeRule(int offsetMinutes, double multiplier) {
+        this.offsetMinutes = offsetMinutes;
+        this.multiplier = multiplier;
+    }
+
+    public int getOffsetMinutes() {
+        return offsetMinutes;
+    }
+
+    public double getMultiplier() {
+        return multiplier;
+    }
+
+    public int toLocalTime(int worldMinutes) {
+        return (int) (worldMinutes * multiplier) + offsetMinutes;
+    }
+
+    public int toWorldTime(int localMinutes) {
+        return (int) ((localMinutes - offsetMinutes) / multiplier);
+    }
+}
