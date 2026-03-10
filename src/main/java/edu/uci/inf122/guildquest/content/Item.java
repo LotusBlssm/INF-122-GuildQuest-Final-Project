a4cc8743594af280705ad6a8964502d670b8920c
Connor Darling <connordarling@me.com>
2026-03-10 12:06:57 -0700

    Add Item class to represent game items with properties such as ID, name, rarity, and description
    - Introduced an abstract Item class to encapsulate common attributes and methods for game items, including unique identification and descriptive properties.

---
+++ src/main/java/edu/uci/inf122/guildquest/content/Item.java
@@ -0,0 +1,33 @@
+package edu.uci.inf122.guildquest.content;
+
+import java.util.UUID;
+
+public abstract class Item {
+    private UUID itemID;
+    private String name;
+    private int rarity;
+    private String description;
+
+    public Item(UUID itemID, String name, int rarity, String description) {
+        this.itemID = itemID;
+        this.name = name;
+        this.rarity = rarity;
+        this.description = description;
+    }
+
+    public UUID getItemID() {
+        return itemID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRarity() {
+        return rarity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
