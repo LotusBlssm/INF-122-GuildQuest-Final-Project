f9dc132d74210accc2d2303f6adc94d596d42a54
Connor Darling <connordarling@me.com>
2026-03-16 13:45:58 -0700

    Refactored MiniAdventure class to use protected access for fields and added a constructor for initialization. Introduced getter methods for realms, entities, win conditions, players, state, and previous save.

--- src/main/java/edu/uci/inf122/guildquest/engine/MiniAdventure.java
+++ src/main/java/edu/uci/inf122/guildquest/engine/MiniAdventure.java
@@ -10,15 +10,36 @@ import edu.uci.inf122.guildquest.entities.Entity;
 import java.util.List;
 
 public abstract class MiniAdventure {
-    private List<Realm> realms;
-    private List<Entity> entities;
-    private List<WinCondition> winCondition;
-    private List<User> players;
-    private State state;
-    private AdventureSnapshot previousSave=null;
+    protected List<Realm> realms;
+    protected List<Entity> entities;
+    protected List<WinCondition> winConditions;
+    protected List<User> players;
+    protected State state;
+    protected AdventureSnapshot previousSave;
+
+    protected MiniAdventure(List<Realm> realms, List<Entity> entities,
+                            List<WinCondition> winConditions, List<User> players,
+                            State state) {
+        this.realms = realms;
+        this.entities = entities;
+        this.winConditions = winConditions;
+        this.players = players;
+        this.state = state;
+        this.previousSave = null;
+    }
+
+    public abstract String getName();
+    public abstract String getDescription();
 
     public abstract void play();
     public abstract void acceptInput();
     public abstract void advanceCycle();
     public abstract AdventureSnapshot saveSnapshot();
+
+    public List<Realm> getRealms() { return realms; }
+    public List<Entity> getEntities() { return entities; }
+    public List<WinCondition> getWinConditions() { return winConditions; }
+    public List<User> getPlayers() { return players; }
+    public State getState() { return state; }
+    public AdventureSnapshot getPreviousSave() { return previousSave; }
 }
